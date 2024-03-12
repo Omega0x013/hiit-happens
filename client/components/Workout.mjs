ff6952dc0a17251ce0e9a27ccbe2153906a37db5
Xander Bielby <xanderbielby@gmail.com>
2024-03-12 10:12:11 +0000

    example timer works

--- client/components/Workout.mjs
+++ client/components/Workout.mjs
@@ -1,2 +1,34 @@
 // Web Component: <workout-component></workout-component>
 // A single workout component that displays the workout name and the exercises in the workout
+
+export default class WorkoutComponent extends HTMLElement {
+    constructor() { 
+        super();
+    }
+
+    async fetchWorkoutData() {
+        try {
+            const res = await fetch(`/api/workout/${this.dataset.id}`);
+            const body = await res.json();
+            this.workout = body;
+            console.log(this.workout);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async connectedCallback() {
+        this.attachShadow({ mode: "open" });
+
+        await this.fetchWorkoutData();
+        
+        this.h2 = Object.assign(document.createElement("h2"), {"textContent": `${this.workout.name}`});
+        this.duration = Object.assign(document.createElement("p"), {"textContent": `Duration: ${Math.floor(this.workout.duration/1000)}`});
+        this.link = Object.assign(document.createElement("a"), {"textContent": "Use this timer", "href": `/timer/?id=${this.dataset.id}`});
+        this.hr = document.createElement("hr");
+
+        this.shadowRoot.append(this.h2, this.duration, this.link, this.hr);
+    }
+}
+
+customElements.define("workout-component", WorkoutComponent);
--- client/script.mjs
+++ client/script.mjs
@@ -1,3 +1,17 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("/serviceworker.js");
 }
+
+const workouts = document.querySelector("#workouts");
+
+try {
+    const res = await fetch(`/api/list/${1}`)
+    const body = await res.json();
+    for (const id of body) {
+        const w = document.createElement("workout-component");
+        w.dataset.id = id;
+        workouts.append(w);
+    }
+} catch (e) {
+    console.error(e.message);
+}
