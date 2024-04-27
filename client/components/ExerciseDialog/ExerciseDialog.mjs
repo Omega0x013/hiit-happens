e138dc6aa28c2a3a699ea0a9abf1789407e9ed18
Xander Bielby <xanderbielby@gmail.com>
2024-04-27 17:41:35 +0000

    removed exercise dialog in favour of inline editing

--- client/components/ExerciseDialog/ExerciseDialog.mjs
+++
@@ -1,8 +0,0 @@
-export default class ExerciseDialog extends HTMLDialogElement {
-  async connectedCallback() {
-    const template = await fetch(import.meta.resolve('./ExerciseDialog.html'));
-    this.innerHTML = await template.text();
-  }
-}
-
-customElements.define('exercise-dialog', ExerciseDialog, { extends: 'dialog' });
