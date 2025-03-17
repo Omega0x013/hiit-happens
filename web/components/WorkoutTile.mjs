beddf1d8e72a770633564d4d156491a040e7cd45
Stuart-Dann <stuartdann03@gmail.com>
2025-03-17 11:47:39 +0000

    Add dropdown button using clip-path for animation
    my prettier is set to " so it has changed all the ones in the files i edited

--- web/components/WorkoutTile.mjs
+++ web/components/WorkoutTile.mjs
@@ -5,40 +5,47 @@
  * - Contains a timer-dialog, allowing the user to run the exercise.
  */
 export default class WorkoutTile extends HTMLElement {
-  connectedCallback() {
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.append(document.querySelector('#workout-tile-template').content.cloneNode(true));
+	connectedCallback() {
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.append(document.querySelector("#workout-tile-template").content.cloneNode(true));
 
-    for (const div of this.shadowRoot.querySelectorAll('div:not(dialog *)')) {
-      const [button, dialog] = div.children;
-      button.addEventListener('click', () => dialog.showModal());
-      dialog.dataset.id = this.dataset.id;
-    }
+		for (const div of this.shadowRoot.querySelectorAll("div:not(dialog *)")) {
+			const [button, dialog] = div.children;
+			button.addEventListener("click", () => dialog.showModal());
+			dialog.dataset.id = this.dataset.id;
+		}
 
-    this.shadowRoot.querySelector('#delete-dialog').addEventListener('close', event => {
-      if (event.target.returnValue === 'Confirm') {
-        localStorage.removeItem(this.dataset.id);
-        this.remove();
-      }
-    });
+		this.shadowRoot.querySelector("#delete-dialog").addEventListener("close", (event) => {
+			if (event.target.returnValue === "Confirm") {
+				localStorage.removeItem(this.dataset.id);
+				this.remove();
+			}
+		});
 
-    this.shadowRoot.addEventListener('update', this.update.bind(this));
-    this.update();
-  }
+		this.shadowRoot.addEventListener("update", this.update.bind(this));
+		this.shadowRoot.querySelector("#dropdown-button").addEventListener("click", this.toggleDropdown.bind(this));
+		this.update();
+	}
 
-  update() {
-    const { name, exercises } = JSON.parse(localStorage.getItem(this.dataset.id));
+	update() {
+		const { name, exercises } = JSON.parse(localStorage.getItem(this.dataset.id));
 
-    this.shadowRoot.querySelector('form p > span').textContent = name;
+		this.shadowRoot.querySelector("form p > span").textContent = name;
 
-    const ms = exercises?.reduce((p, c) => p + c.duration, 0);
-    const duration = `${Math.floor(ms / 1000 / 60)}m ${Math.floor(ms / 1000 % 60)}s`;
-    this.shadowRoot.querySelector('h2').textContent = `${name} - ${duration}`;
+		const ms = exercises?.reduce((p, c) => p + c.duration, 0);
+		const duration = `${Math.floor(ms / 1000 / 60)}m ${Math.floor((ms / 1000) % 60)}s`;
+		this.shadowRoot.querySelector("h2").textContent = `${name} - ${duration}`;
 
-    // When this tile is updated, call the child event handlers without bubbling
-    const event = new Event('update', { bubbles: false });
-    for (const dialog of this.shadowRoot.querySelectorAll('dialog')) {
-      dialog.dispatchEvent(event);
-    }
-  }
+		// When this tile is updated, call the child event handlers without bubbling
+		const event = new Event("update", { bubbles: false });
+		for (const dialog of this.shadowRoot.querySelectorAll("dialog")) {
+			dialog.dispatchEvent(event);
+		}
+	}
+
+	toggleDropdown() {
+		this.shadowRoot.querySelector("#workout-tile-fieldset").classList.toggle("hidden");
+		this.shadowRoot.querySelector("#workout-tile-fieldset").classList.toggle("visible");
+		this.shadowRoot.querySelector("#dropdown-button").textContent = this.shadowRoot.querySelector("#workout-tile-fieldset").classList.contains("hidden") ? "Expand" : "Collapse";
+	}
 }
